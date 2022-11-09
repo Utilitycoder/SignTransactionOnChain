262a697070831e275c406512f99d8b38b5fe7171
Utilitycoder <108197698+Lawalrowship@users.noreply.github.com>
2022-11-09 18:43:47 +0100

    Task: updated IERC20Permit

--- contracts/IERC20Permit.sol
+++ contracts/IERC20Permit.sol
@@ -20,11 +20,20 @@ interface IERC20Permit {
     function transfer(address recipient, uint amount) external returns (bool);
 
     /**
-    *@dev permits contract to spend on behalf of users
+    *@dev check how much a contract is allowed to spend
     *@param owner: account to spend on behalf
     *@param spender: account that spends owner's fund 
     */
     function allowance(address owner, address spender) external view returns (uint);
+
+    /** 
+    *@dev approve contract to spend certain amount
+    *@param spender: the spender address
+    *@param amount: amount to be spent 
+    */
+    function approve(address spender, uint amount) external returns (bool);
+
+    
 
 
 
